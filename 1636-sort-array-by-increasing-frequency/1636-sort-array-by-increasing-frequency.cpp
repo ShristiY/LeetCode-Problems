38d0825e489a50d64c7bd2b6ca26ee12d1fa6535
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-05-30 19:16:44 +0530

    Time: 10 ms (53.43%), Space: 11.5 MB (36.74%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    struct compare{
+    bool operator()(pair<int,int>a,pair<int,int>b){
+        if(a.first==b.first){
+            return a.second<b.second;
+        }
+        return a.first>b.first;
+    }
+    };
+    vector<int> frequencySort(vector<int>& nums) {
+        vector<int>ans;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,compare>pq;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+            mp[nums[i]]++;
+        
+        for(auto it:mp)
+        {
+            int ele=it.first;
+            int freq=it.second;
+            pq.push({freq,ele});
+        }
+        
+        while(!pq.empty())
+        {
+            auto it=pq.top();
+            int freq=it.first;
+            int num=it.second;
+            while(freq>0)
+            {
+                ans.push_back(num);
+                freq--;
+            }
+            pq.pop();
+        }
+        return ans;
+    }
+};
