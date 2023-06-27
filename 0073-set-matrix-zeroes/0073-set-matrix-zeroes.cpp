826920f8e56d0972341891772ae0028a2e0e3947
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-06-27 15:55:15 +0530

    Time: 14 ms (79.80%), Space: 13.2 MB (50.96%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    //Most optimal
+    //IInd Approach
+    
     void setZeroes(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
         
-        int flag=0;
-        
+        vector<int>dumR(m,-1);
+        vector<int>dumC(n,-1);
         for(int i=0;i<n;i++)
         {
-            if(mat[i][0]==0) flag=1;
-            for(int j=1;j<m;j++)
+            for(int j=0;j<m;j++)
             {
                 if(mat[i][j]==0)
                 {
-                    mat[i][0]=0;
-                    mat[0][j]=0;
+                    dumR[j]=0;
+                    dumC[i]=0;
                 }
             }
         }
-        
-        for(int i=n-1;i>=0;i--)
+        for(int i=0;i<n;i++)
         {
-            for(int j=m-1;j>=1;j--)
+            for(int j=0;j<m;j++)
             {
-                if(mat[i][0]==0 || mat[0][j]==0)
+                if(dumR[j]==0 || dumC[i]==0)
                     mat[i][j]=0;
             }
-            if(flag==1)
-                mat[i][0]=0;
         }
-    
     }
 };
