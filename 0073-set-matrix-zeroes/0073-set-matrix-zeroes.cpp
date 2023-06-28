2c0380647b8598a50d4cd8febc44e7e67c32900e
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-06-28 08:47:51 +0530

    Time: 16 ms (71.36%), Space: 13.2 MB (84.19%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
-        int n=mat.size();
+       int n =mat.size();
         int m=mat[0].size();
-        
         int col0=1;
         
         for(int i=0;i<n;i++)
@@ -33,17 +32,13 @@ public:
         if(mat[0][0]==0)
         {
             for(int i=0;i<m;i++)
-            {
                 mat[0][i]=0;
-            }
         }
+        
         if(col0==0)
         {
-            for(int j=0;j<n;j++)
-            {
-                
-                mat[j][0]=0;
-            }
+            for(int i=0;i<n;i++)
+                mat[i][0]=0;
         }
     }
 };
