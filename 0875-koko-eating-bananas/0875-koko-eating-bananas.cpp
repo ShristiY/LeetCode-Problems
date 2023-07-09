7f8d4cfd52cb7dbcd46eff90c1b1cece3e3c30e2
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-07-09 11:23:07 +0530

    Time: 63 ms (39.91%), Space: 18.9 MB (37.72%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    int maxElement(vector<int>& arr){
+        int maxi = INT_MIN;
+        for(int i=0; i<arr.size(); i++){
+            maxi = max(maxi, arr[i]);
+        }
+        return maxi;
+    }
+
+    long long calculate_totalhrs(vector<int>& arr, int hourly){
+        long long totalhrs = 0;
+        for(int i=0; i<arr.size(); i++){
+            totalhrs += (arr[i] + hourly - 1) / hourly;
+        }
+        return totalhrs;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1; 
+        int high = maxElement(piles);
+        while(low < high){
+            int mid = low + (high-low)/2;
+            int totalhrs = calculate_totalhrs(piles, mid);
+
+            if(totalhrs <= h){
+                high = mid;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
