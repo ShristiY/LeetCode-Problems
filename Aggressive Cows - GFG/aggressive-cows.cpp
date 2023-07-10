8c935d82cc9b9b9f78d4ef3351db68d2c738d483
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-07-10 12:32:25 +0530

    Added solution - LeetHub

---
+++ Aggressive Cows - GFG/aggressive-cows.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+
+    bool ispossible( vector<int> &stalls , int n , int k , int mid){
+         
+         int cntcows = 1;
+         int lastplacedcow =stalls[0];
+         
+         for(int i = 1; i < n ; i++){
+             
+             if( stalls[i] - lastplacedcow >= mid){
+                 
+                 cntcows++;
+                 lastplacedcow = stalls[i];
+             }
+         }
+         
+         if(cntcows >= k)
+         return true;
+         
+         else
+         return false;
+     }
+    int solve(int n, int k, vector<int> &stalls) {
+    
+            sort(stalls.begin() , stalls.end());
+       
+        int low = 1;
+        int high = stalls[n-1] - stalls[0];
+        
+        while( low <= high){
+            
+            int mid = (low + high) >> 1;
+            
+            if( ispossible( stalls , n , k , mid)){
+                
+                low = mid + 1;
+            }
+            
+            else{
+                high = mid -1;
+            }
+            
+            
+        }
+        
+        return high;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> stalls(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stalls[i];
+        }
+        // char ch;
+        // cin >> ch;
+
+        Solution obj;
+        cout << obj.solve(n, k, stalls) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
