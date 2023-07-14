9b1a0168c3acb3ad2a91437ee06c448291686429
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-07-14 11:32:18 +0530

    Time: 24 ms (91.26%), Space: 22.3 MB (77.00%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,25 +5,24 @@ public:
         if(n==1) return nums[0];
         if(nums[0]!=nums[1]) return nums[0];
         if(nums[n-1]!=nums[n-2]) return nums[n-1];
-        
-        int l=1,h=n-2;
+         int l=1,h=n-2;
         while(l<=h)
         {
             int mid=l+(h-l)/2;
             if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
                 return nums[mid];
+            
             else if(mid%2==0)
             {
-                if(nums[mid-1]==nums[mid])
+                if(nums[mid]==nums[mid-1])
                     h=mid-1;
                 else
                     l=mid+1;
             }
-            else if(mid%2==1)
-            {
-                if(nums[mid]==nums[mid-1]) 
+            else {
+                if(nums[mid]==nums[mid-1])
                     l=mid+1;
-                else 
+                else
                     h=mid-1;
             }
         }
