867fc61f196c8cf608279f6d9a47f5841c85332b
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-07-14 16:30:24 +0530

    Time: 217 ms (45.48%), Space: 66.5 MB (93.22%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
-    int findMin(vector<int>&arr)
+    bool func(int day,vector<int>&arr,int m,int k)
     {
-        int ans=INT_MAX;
-        for(int i=0;i<arr.size();i++)
-            ans=min(ans,arr[i]);
-        return ans;
-    }
-    int findMax(vector<int>&arr)
-    {
-        int ans=INT_MIN;
-        for(int i=0;i<arr.size();i++)
-            ans=max(ans,arr[i]);
-        return ans;
-    }
-    bool func(vector<int>&arr,int day,int m,int k)
-    {
-        int bouquets=0,flowers=0;
+        int flowers=0,bouquets=0;
+        
         for(int i=0;i<arr.size();i++)
         {
             if(arr[i]<=day)
@@ -30,19 +17,26 @@ public:
         if(bouquets>=m) return true;
         return false;
     }
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        int l=findMin(bloomDay),h=findMax(bloomDay);
+    int minDays(vector<int>& arr, int m, int k) {
+        int mini=INT_MAX;
+        int maxi=INT_MIN;
+        for(int i=0;i<arr.size();i++)
+        {
+            maxi=max(maxi,arr[i]);
+            mini=min(mini,arr[i]);
+        }
         int ans=-1;
+        int l=mini,h=maxi;
         while(l<=h)
         {
             int mid=l+(h-l)/2;
-            if(func(bloomDay,mid,m,k)==true)
+            if(func(mid,arr,m,k)==true)
             {
-                h=mid-1;
                 ans=mid;
+                h=mid-1;
             }
             else
-               l=mid+1;
+                l=mid+1;
         }
         return ans;
     }
