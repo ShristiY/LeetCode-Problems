2efe477c43d677ea8fcfd0f900989905bb916a9d
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-07-14 15:51:11 +0530

    Time: 51 ms (68.78%), Space: 19 MB (37.34%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    
-    int maxElement(vector<int>& arr){
-        int maxi = INT_MIN;
-        for(int i=0; i<arr.size(); i++){
-            maxi = max(maxi, arr[i]);
-        }
-        return maxi;
+    long long func(int mid,vector<int>&arr,int h)
+    {
+        long long total=0;
+        for(int i=0;i<arr.size();i++)
+            total+=ceil((double(arr[i])/double(mid)));
+        return total;
     }
-
-    long long calculate_totalhrs(vector<int>& arr, int hourly){
-        long long totalhrs = 0;
-        for(int i=0; i<arr.size(); i++){
-            totalhrs += (arr[i] + hourly - 1) / hourly;
-        }
-        return totalhrs;
-    }
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1; 
-        int high = maxElement(piles);
-        int ans=maxElement(piles);
-        while(low < high){
-            int mid = low + (high-low)/2;
-            int totalhrs = calculate_totalhrs(piles, mid);
-
-            if(totalhrs <= h){
+    int minEatingSpeed(vector<int>& arr, int h) {
+        int n=arr.size();
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++)
+            maxi=max(maxi,arr[i]);
+        
+        int l=1,high=maxi;
+        int ans=maxi;
+        while(l<=high)
+        {
+            int mid=l+(high-l)/2;
+            if(func(mid,arr,h)<=h)
+            {
                 ans=mid;
-                high = mid;
+                high=mid-1;
             }
-            else{
-                low = mid + 1;
-            }
+            else
+                l=mid+1;
         }
-
         return ans;
     }
 };
