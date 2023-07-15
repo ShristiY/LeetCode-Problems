cee02a7cdd8044b12dd53d8c5c7b8874dc408e71
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-07-15 18:41:27 +0530

    Time: 311 ms (44.26%), Space: 75.6 MB (32.42%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>>ans;
-        priority_queue<pair<int,pair<int,int>>>pq;
-        for(int i=0;i<points.size();i++)
+        priority_queue<pair<int,pair<int,int>>>maxh;
+        for(auto it:points)
         {
-            int x=points[i][0];
-            int y=points[i][1];
-            int dist=x*x+y*y;
-            
-            pq.push({dist,{x,y}});
-            if(pq.size()>k)
-                pq.pop();
+            int x=it[0]*it[0];
+            int y=it[1]*it[1];
+            int res=x+y;
+            maxh.push({res,{it[0],it[1]}});
+            if(maxh.size()>k)
+                maxh.pop();
         }
-        
-        while(!pq.empty())
+        while(!maxh.empty())
         {
-            int dist=pq.top().first;
-            int x=pq.top().second.first;
-            int y=pq.top().second.second;
-            pq.pop();
-            vector<int>vec;
-            vec.push_back(x);
-            vec.push_back(y);
-            ans.push_back(vec);
+            int res=maxh.top().first;
+            int xaxis=maxh.top().second.first;
+            int yaxis=maxh.top().second.second;
+            maxh.pop();
+            vector<int>temp;
+            temp.push_back(xaxis);
+            temp.push_back(yaxis);
+            ans.push_back(temp);
         }
         return ans;
     }
