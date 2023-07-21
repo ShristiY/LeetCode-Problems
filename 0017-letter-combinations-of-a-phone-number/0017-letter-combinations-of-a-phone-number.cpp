3a30ff42250747d1b54e016d01ae58f317236249
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-07-21 16:40:55 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (55.98%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-
-//THIS CODE IS UNDERSTANDABLE(BY LOVE BABBAR) NOT THE PREVIOUS ONE
-void func(vector<string>&ans,string ds,vector<string>&mp,string digits,int ind)
-{
-    if(ind==digits.size())
+    void func(string digits, vector<string>&ans, string ds, vector<string>&mp, int index)
     {
-        ans.push_back(ds);
-        return;
+        if(index==digits.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+        
+        int currNum=digits[index]-'0'; // to convert from string to integer.
+        string value=mp[currNum];
+        for(int i=0;i<value.size();i++)
+        {
+            ds.push_back(value[i]);
+            func(digits,ans,ds,mp,index+1);
+            ds.pop_back();
+        }
     }
-    int currNum=digits[ind]-'0'; // to convert string to integer
-    string val=mp[currNum];
-    for(int i=0;i<val.size();i++)
-    {
-        ds.push_back(val[i]);
-        func(ans,ds,mp,digits,ind+1);
-        ds.pop_back();
-    }
-}
     vector<string> letterCombinations(string digits) {
         vector<string>ans;
         string ds;
-        if(digits.size()==0)
-        return ans;
+        int n=digits.size();
+        if(n==0)
+            return ans;
         vector<string>mp={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        func(ans,ds,mp,digits,0);
+        func(digits,ans,ds,mp,0);
         return ans;
     }
 };
