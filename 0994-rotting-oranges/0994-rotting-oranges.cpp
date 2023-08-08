d2e48f9593bdf4c0fad7761c5620feca445361c9
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-08 15:24:09 +0530

    Time: 0 ms (100.00%), Space: 13.4 MB (20.45%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int fresh=0;
-        int cnt=0;
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        int count=0, freshCount=0;
         queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==2)
                 {
-                    vis[i][j]=2;
                     q.push({{i,j},0});
+                    vis[i][j]=2;
                 }
-                if(grid[i][j]==1)
-                    fresh++;                    
+                 else if(grid[i][j]==1)
+                    freshCount++;
             }
         }
         int time=0;
         int delRow[]={-1,0,1,0};
         int delCol[]={0,1,0,-1};
-        while(!q.empty())
-        {
-            int row=q.front().first.first;
-            int col=q.front().first.second;
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
             int tm=q.front().second;
-            q.pop();
             time=max(time,tm);
+            q.pop();
             for(int i=0;i<4;i++)
             {
-                int nrow=row+delRow[i];
-                int ncol=col+delCol[i];
-                
+                int nrow=r+delRow[i];
+                int ncol=c+delCol[i];
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && !vis[nrow][ncol])
                 {
-                    vis[nrow][ncol]=2;
-                    cnt++;
+                    count++;
+                    vis[nrow][ncol]=1;
                     q.push({{nrow,ncol},tm+1});
                 }
-            }  
+            }
         }
-        if(cnt==fresh) return time;
+        if(count==freshCount) return time;
         return -1;
     }
 };
