4f9d46350b2269014fe411d6647585aa5cdb1a2a
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-08 11:35:02 +0530

    Time: 23 ms (98.65%), Space: 13.3 MB (48.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-void dfs(int x, int y, vector<vector<int>>& vis, vector<vector<char>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-    if (x < 0 || x >= n || y < 0 || y >= m || vis[x][y] == 1 || grid[x][y] == '0') {
-        return;
-    }
-    vis[x][y] = 1;
-    dfs(x + 1, y, vis, grid);
-    dfs(x - 1, y, vis, grid);
-    dfs(x, y + 1, vis, grid);
-    dfs(x, y - 1, vis, grid);
-}
-
-    int numIslands(vector<vector<char>>& grid) {
-        int cnt=0;
+    void dfs(int r, int c, vector<vector<char>>&grid, vector<vector<int>>&vis)
+    {
+        vis[r][c]=1;
         int n=grid.size();
         int m=grid[0].size();
+        int delRow[]={-1,0,1,0};
+        int delCol[]={0,1,0,-1};
+        for(int i=0;i<4;i++)
+        {
+            int nrow=r+delRow[i];
+            int ncol=c+delCol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol])
+            {
+                dfs(nrow,ncol,grid,vis);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
         if(n==0) return 0;
+        int m=grid[0].size();
+        int count=0;
         vector<vector<int>>vis(n,vector<int>(m,0));
-        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if (grid[i][j]=='1' && !vis[i][j])
+                if(grid[i][j]=='1' && vis[i][j]==0)
                 {
-                    cnt++;
-                    dfs(i,j,vis,grid);
+                    count++;
+                    dfs(i,j,grid,vis);
                 }
             }
         }
-        return cnt;
+        return count;
     }
 };
