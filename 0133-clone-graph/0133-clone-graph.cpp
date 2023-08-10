fecd7175ac7a6810d6011adfc5e664d0829a81bf
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-10 15:12:08 +0530

    Time: 3 ms (86.41%), Space: 9.2 MB (16.43%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,26 +21,28 @@ public:
 
 class Solution {
 public:
-    Node* dfs(Node* cur, unordered_map<Node*,Node*>&mp)
+    //WATCH AYUSHI - THIS IS DFS TC:- O(V+E) SC:-O(V)
+    Node* dfs(Node* cur, unordered_map<Node*,Node*>&mp)//CUR IS ORIGINAL NODE
     {
-        vector<Node*>cloneAdj;
+        vector<Node*>cloneAdj;//CREATE ADJ LIST FOR CLONE GRAPH
         Node* clone=new Node(cur->val);
-        mp[cur]=clone;
-        for(auto it:cur->neighbors)
+        mp[cur]=clone;//PUT THE ORIGINAL AND CLONE NODES AS KEY-VALUE PAIRS IN THE MAP
+        for(auto it:cur->neighbors)//TRAVERSE THE ADJ LIST OF ORIGIANL GRAPH
         {
-            if(mp.find(it)!=mp.end())
-                cloneAdj.push_back(mp[it]);
+            if(mp.find(it)!=mp.end())//IF MAP ALREADY CONTAINS THE GIVEN NODE
+                cloneAdj.push_back(mp[it]); //THEN USE THE EXISTING ONE
             else
-                cloneAdj.push_back(dfs(it,mp));
+                cloneAdj.push_back(dfs(it,mp));//IF IT DOESN'T CONTAIN THEN CALL DFS FOR THAT PARTICULAT NODE AND CREATE THAT NODE AND THAT KEY-VALUE PAIR
         }
-        clone->neighbors=cloneAdj;
+        clone->neighbors=cloneAdj;//IN THE END JUST MAKE THE neighbours (which is the default adj list of our clone graph (done by constructor)) equal to the ADJ list CLONEADJ that we just now created
         return clone;
         
     }
     Node* cloneGraph(Node* node) {
         unordered_map<Node*,Node*>mp;
         if(node==NULL) return NULL;
-        if(node->neighbors.size()==0) {
+        if(node->neighbors.size()==0)//if there is only one node that means there will be no neighbors that means adj list will be empty
+        {
             Node* clone=new Node(node->val);
             return clone;
         }
