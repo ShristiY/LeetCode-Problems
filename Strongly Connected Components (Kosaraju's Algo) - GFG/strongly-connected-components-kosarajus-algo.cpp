62f921b2afad3ddbebc7295b13ffb27cdaf32b57
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-10 11:59:12 +0530

    Added solution - LeetHub

---
+++ Strongly Connected Components (Kosaraju's Algo) - GFG/strongly-connected-components-kosarajus-algo.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find number of strongly connected components in the graph.
+	void dfs(int node,vector<int>&vis,stack<int>&st,vector<vector<int>>&adj)
+	{
+	    vis[node]=1;
+	    for(auto it:adj[node])
+	    {
+	        if(!vis[it])
+	        dfs(it,vis,st,adj);
+	    }
+	    st.push(node);
+	}
+	
+	void dfs3(int node, vector<int>&vis, vector<vector<int>>&adjT)
+	{
+	    vis[node]=1;
+	    for(auto it:adjT[node])
+	    {
+	        if(!vis[it])
+	        dfs3(it,vis,adjT);
+	    }
+	}
+    int kosaraju(int V, vector<vector<int>>& adj)
+    {
+        vector<int>vis(V,0);
+        stack<int>st;
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            dfs(i,vis,st,adj);
+        }
+        
+        //REVERSE NODES
+       
+        vector<vector<int>> adjT(V); // Note: This should be a vector of vectors, not an array
+for(int i = 0; i < V; i++)
+{
+    vis[i] = 0;
+    for(auto it : adj[i])
+    {
+        adjT[it].push_back(i); // Reverse the direction, but keep the edge
+    }
+}
+
+        
+        int scc=0;
+       while(!st.empty())
+       {
+           int node=st.top();
+           st.pop();
+           
+               if(!vis[node])
+               {
+                   scc++;
+                   dfs3(node,vis,adjT);
+               }
+           
+       }
+        return scc;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<vector<int>> adj(V);
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.kosaraju(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
