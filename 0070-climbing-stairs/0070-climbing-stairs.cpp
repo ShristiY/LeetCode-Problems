a28f2788d6980703303395631aadc16b0c694871
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-11 20:16:15 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (14.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     int func(vector<int>&dp,int n)
     {
-        if(n<=1) return 1;
+        dp[0]=1;
+        dp[1]=1;
         if(dp[n]!=-1) return dp[n];
-        return dp[n]=func(dp,n-1)+func(dp,n-2);
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
     int climbStairs(int n) {
-        //memoization
+        //tabulation
         vector<int>dp(n+1,-1);
         return func(dp,n);
     }
