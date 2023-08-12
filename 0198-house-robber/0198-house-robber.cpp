b356fe7c5ba8537123db97f7013c975924adeaa9
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-12 15:03:50 +0530

    Time: 3 ms (38.09%), Space: 7.9 MB (15.81%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    //recursion
+    int func(int index,vector<int>&nums, vector<int>&dp)
+    {
+        if(index==0) return nums[index];
+        if(index<0) return 0;
+        if(dp[index]!=-1) return dp[index];
+       
+            int pick=nums[index];
+            if(index>1)
+            pick+=func(index-2,nums,dp);
+            int notPick=0+func(index-1,nums,dp);
+        
+        return dp[index]= max(pick,notPick);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return func(n-1,nums,dp);
+    }
+};
