f300908a0fea2b2ba0e6d15bb83858d8073f2fc5
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-12 15:10:49 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (74.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    //tabulation
-    int func(int index,vector<int>&nums, vector<int>&dp)
-    {
-        dp[0]=nums[0];
-        
-        if(dp[index]!=-1) return dp[index];
-       
-        for(int i=1;i<=index;i++)
+    //MOST OPTIMISED
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++)
         {
             int pick=nums[i];
             if(i>1)
-            pick+=dp[i-2];
-            int notPick=0+dp[i-1];
-            dp[i]=max(pick,notPick);
+                pick+=prev2;
+            int notPick=prev;
+            int curr=max(pick,notPick);
+            prev2=prev;
+            prev=curr;
         }
-        
-        return dp[index];
-    }
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-        return func(n-1,nums,dp);
+        return prev;
     }
 };
