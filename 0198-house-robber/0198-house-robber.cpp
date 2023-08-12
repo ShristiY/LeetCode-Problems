3f5d350f9276247de5057eda37090adfc22b0b99
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-12 15:07:19 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (15.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    //memoization
+    //tabulation
     int func(int index,vector<int>&nums, vector<int>&dp)
     {
-        if(index==0) return nums[index];
-        if(index<0) return 0;
+        dp[0]=nums[0];
+        
         if(dp[index]!=-1) return dp[index];
        
-            int pick=nums[index];
-            if(index>1)
-            pick+=func(index-2,nums,dp);
-            int notPick=0+func(index-1,nums,dp);
+        for(int i=1;i<=index;i++)
+        {
+            int pick=nums[i];
+            if(i>1)
+            pick+=dp[i-2];
+            int notPick=0+dp[i-1];
+            dp[i]=max(pick,notPick);
+        }
         
-        return dp[index]= max(pick,notPick);
+        return dp[index];
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
