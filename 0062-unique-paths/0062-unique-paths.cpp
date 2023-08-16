f7020b2a8fec4aed51d65ecd3a1e8f879412df8e
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-16 10:49:18 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (21.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //TABULATION
+    //MOST OPTIMISED
     int func(int n, int m, vector<vector<int>>&dp)
     {
         for(int i=0;i<n;i++)
@@ -10,14 +10,16 @@ public:
                 if(i==0 && j==0)
                 {
                     dp[i][j]=1;
-                    continue;
+                    
                 }
+                else{
                 int up=0,left=0;
                 if(i>0)
                 up=dp[i-1][j];
                 if(j>0)
                 left=dp[i][j-1];
                 dp[i][j]=up+left;
+                }
             }
         }
         return dp[n-1][m-1];
