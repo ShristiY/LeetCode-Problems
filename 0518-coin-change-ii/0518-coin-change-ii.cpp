84ea6230cc19bace3a4d1720390b52e1078eaed6
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-18 15:43:01 +0530

    Time: 41 ms (22.99%), Space: 29.5 MB (6.11%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    long countWaysToMakeChangeUtil(vector<int>& arr,int ind, int T, vector<vector<long
->>& dp){
-
-    if(ind == 0){
-        if(T%arr[0]==0)
-            return 1;
-        return 0;
-    }
-    
-    if(dp[ind][T]!=-1)
-        return dp[ind][T];
-        
-    long notTaken = countWaysToMakeChangeUtil(arr,ind-1,T,dp);
-    
-    long taken = 0;
-    if(arr[ind] <= T)
-        taken = countWaysToMakeChangeUtil(arr,ind,T-arr[ind],dp);
-        
-    return dp[ind][T] = notTaken + taken;
-}
 
     int change(int T, vector<int>& arr) {
         int n=arr.size();
-         vector<vector<long>> dp(n,vector<long>(T+1,-1));
-    return countWaysToMakeChangeUtil(arr,n-1, T, dp);
+         vector<vector<long>> dp(n,vector<long>(T+1,0));
+    for(int i=0;i<=T;i++)
+    {
+        if(i%arr[0]==0)
+            dp[0][i]=1;
+    }
+        
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int target=0;target<=T;target++)
+            {
+                long notTake=dp[ind-1][target];
+                long take=0;
+                if(arr[ind]<=target)
+                    take=dp[ind][target-arr[ind]];
+                
+                dp[ind][target]=take+notTake;
+            }
+        }
+        return dp[n-1][T];
     }
 };
