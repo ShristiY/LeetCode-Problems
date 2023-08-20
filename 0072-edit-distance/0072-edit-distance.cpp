b8523831a832df9f2fb1b0bbd9c78a0ffe4eb66c
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-20 19:34:10 +0530

    Time: 14 ms (55.64%), Space: 9 MB (39.89%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int func(string s1,string s2, int i, int j, vector<vector<int>>&dp)
-    {
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        if(s1[i]==s2[j])
-            return dp[i][j]= func(s1,s2,i-1,j-1,dp);
-        else
-            return dp[i][j]=1+min(func(s1,s2,i-1,j-1,dp),min(func(s1,s2,i-1,j,dp),func(s1,s2,i,j-1,dp)));
-    }
+
     int minDistance(string s1, string s2) {
         int n=s1.size();
         int m=s2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return func(s1,s2,n-1,m-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++)
+            dp[i][0]=i;
+        for(int i=0;i<=m;i++)
+            dp[0][i]=i;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+            }
+        }
+        return dp[n][m];
     }
 };
