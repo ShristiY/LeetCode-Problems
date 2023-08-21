ac27eef6d0784b0d07d129ea1c8820a5201b9158
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-21 20:24:09 +0530

    Time: 519 ms (55.30%), Space: 207 MB (47.63%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int func(int ind,int buy,int cap,vector<int>&arr, int n, vector<vector<vector<int>>>&dp)
-    {
-        if(cap==0 || ind==n) return 0;
-        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
-        int profit;
-        if(buy==0)
-            profit=max(-arr[ind]+func(ind+1,1,cap,arr,n,dp),func(ind+1,0,cap,arr,n,dp));
-        else
-            profit=max(func(ind+1,1,cap,arr,n,dp),arr[ind]+func(ind+1,0,cap-1,arr,n,dp));
-        
-        return dp[ind][buy][cap]=profit;
-            
+    int maxProfit(vector<int>& Arr) {
+        int n=Arr.size();
+        vector<vector<vector<int>>> dp(n+1,
+                                    vector<vector<int>> 
+                                            (2,vector<int>(3,0)));
+                                            
+    // As dp array is intialized to 0, we have already covered the base case
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy = 0; buy<=1; buy++){
+            for(int cap=1; cap<=2; cap++){
+                
+                if(buy==0){// We can buy the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][0][cap], 
+                                -Arr[ind] + dp[ind+1][1][cap]);
+                 }
+    
+                if(buy==1){// We can sell the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][1][cap],
+                                Arr[ind] + dp[ind+1][0][cap-1]);
+                }
+            }
+        }
     }
-    int maxProfit(vector<int>& arr) {
-        int n=arr.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return func(0,0,2,arr,n,dp);
+    
+    
+    return dp[0][0][2];
     }
 };
