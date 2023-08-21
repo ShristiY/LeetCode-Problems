0433fc0419f5ce37330ab2c18783d922082e9c97
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-21 16:29:52 +0530

    Time: 7 ms (65.14%), Space: 17.6 MB (7.79%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -19,7 +19,7 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        if(n==0) return 0;
+        // if(n==0) return 0;
         vector<vector<int>>dp(n,vector<int>(2,-1));
         return func(0,0,prices,n,dp);
     }
