d1376520102a5357a34be3750f7cd96afb2f92c9
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-22 11:25:52 +0530

    Time: 269 ms (68.22%), Space: 135.2 MB (62.34%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    int func(int ind, int trans, vector<vector<int>>&dp,vector<int>&arr,int n)
-    {
-        if(ind==n) return 0;
-        if(trans==4) return 0;
-        if(dp[ind][trans]!=-1) return dp[ind][trans];
-        int profit;
-        
-        if(trans%2==0)
-        {
-            profit=max(-arr[ind]+func(ind+1,trans+1,dp,arr,n),func(ind+1,trans,dp,arr,n));
-            
-        }
-        else
-            profit=max(arr[ind]+func(ind+1,trans+1,dp,arr,n),func(ind+1,trans,dp,arr,n));
-        
-        return dp[ind][trans]=profit;
-    }
+ 
     int maxProfit(vector<int>& arr) {
         int n=arr.size();
-        vector<vector<int>>dp(n+1,vector<int>(5,-1));
-        return func(0,0,dp,arr,n);
+        vector<vector<int>>dp(n+1,vector<int>(5,0));
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            for(int trans=3;trans>=0;trans--)
+            {
+                if(trans%2==0)
+                    dp[ind][trans]=max(-arr[ind]+dp[ind+1][trans+1],dp[ind+1][trans]);
+                else
+                    dp[ind][trans]=max(arr[ind]+dp[ind+1][trans+1],dp[ind+1][trans]);
+            }
+        }
+        return dp[0][0];
     }
 };
