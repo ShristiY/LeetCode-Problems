baf38590189be0720237f2478bba006155b40b28
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-22 10:47:10 +0530

    Time: 158 ms (87.34%), Space: 75.2 MB (87.60%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,17 +3,19 @@ public:
   
     int maxProfit(vector<int>&  arr) {
         int n=arr.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>>ahead(2,vector<int>(3,0)),cur(2,vector<int>(3,0));
+        
         for(int ind=n-1;ind>=0;ind--)
         {
             for(int cap=1;cap<=2;cap++)
             {
-                dp[ind][0][cap]=max(-arr[ind]+dp[ind+1][1][cap],0+dp[ind+1][0][cap]);
+                cur[0][cap]=max(-arr[ind]+ahead[1][cap],0+ahead[0][cap]);
                 
-                dp[ind][1][cap]=max(arr[ind]+dp[ind+1][0][cap-1],0+dp[ind+1][1][cap]);
+                cur[1][cap]=max(arr[ind]+ahead[0][cap-1],0+ahead[1][cap]);
                 
             }
+            ahead=cur;
         }
-        return dp[0][0][2];
+        return ahead[0][2];
     }
 };
