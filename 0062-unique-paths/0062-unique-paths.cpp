4cf9193dc261c07bf815cb1c3ac8644f9f61a0bf
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-25 13:25:02 +0530

    Time: 3 ms (34.24%), Space: 6.7 MB (5.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    //MOST OPTIMISED
-    int func(int n, int m, vector<vector<int>>&dp)
-    {
+    int uniquePaths(int n, int m) {
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(i==0 && j==0)
-                {
                     dp[i][j]=1;
-                    
-                }
                 else{
-                int up=0,left=0;
-                if(i>0)
-                up=dp[i-1][j];
-                if(j>0)
-                left=dp[i][j-1];
-                dp[i][j]=up+left;
+                    int up=0,left=0;
+                    if(i>0)
+                        up=dp[i-1][j];
+                    if(j>0)
+                        left=dp[i][j-1];
+                    dp[i][j]=up+left;
                 }
             }
         }
         return dp[n-1][m-1];
-    }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return func(m,n,dp);
     }
 };
