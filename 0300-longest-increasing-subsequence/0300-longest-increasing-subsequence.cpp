d6e1852ac5c0b07a8cc54eac387afd6773c9a022
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-28 22:22:37 +0530

    Time: 874 ms (21.43%), Space: 292.2 MB (16.95%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int func(vector<int>&nums,vector<vector<int>>&dp,int ind, int prev, int n)
+    int func(vector<int>&nums,int n, int ind, int prev,vector<vector<int>>&dp)
     {
         if(ind==n) return 0;
         if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
         
-        int notTake=0+func(nums,dp,ind+1,prev,n);
+        int notTake=func(nums,n,ind+1,prev,dp);
         int take=0;
         if(prev==-1 || nums[ind]>nums[prev])
-            take=1+func(nums,dp,ind+1,ind,n);
+            take=1+func(nums,n,ind+1,ind,dp);
         
         return dp[ind][prev+1]=max(take,notTake);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return func(nums,dp,0,-1,n);
+        return func(nums,n,0,-1,dp);
     }
 };
