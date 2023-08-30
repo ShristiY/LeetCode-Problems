660ed8f793ceecd46585ee01075d2d8df7144eeb
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-08-30 15:40:45 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,25 +9,28 @@ using namespace std;
 
 class Solution{
 public:
-int func(int arr[],int i ,int j,vector<vector<int>>&dp)
-{
-    if(i==j) return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
-    
-    int mini=INT_MAX;
-    for(int k=i;k<=j-1;k++)
-    {
-        int ans=arr[i-1]*arr[k]*arr[j]+func(arr,i,k,dp)+func(arr,k+1,j,dp);
-        mini=min(mini,ans);
-    }
-    return dp[i][j]=mini;
-}
     int matrixMultiplication(int N, int arr[])
     {
-        int i=1;
-        int j=N-1;
+        // code here
         vector<vector<int>>dp(N,vector<int>(N,-1));
-        return func(arr,i,j,dp);
+        
+        for(int i=1;i<N;i++) dp[i][i]=0;
+        
+        
+        for(int i=N-1;i>=1;i--)
+        {
+            for(int j=i+1;j<N;j++)
+            {
+                int mini=INT_MAX;
+                for(int k=i;k<j;k++)
+                {
+                    int ans=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                    mini=min(ans,mini);
+                    dp[i][j]=mini;
+                }
+            }
+        }
+        return dp[1][N-1];
     }
 };
 
