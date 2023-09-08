1a042dd33c3dd2bfb50bede3b47ceeb607fa879c
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-09-08 13:46:56 +0530

    Time: 11 ms (71.72%), Space: 13.5 MB (37.10%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,27 +4,40 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         
-        vector<int>d1(n,0),d2(m,0);
+        int col=1;
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(mat[i][j]==0)
-                {
-                    d1[i]=1;
-                    d2[j]=1;
+                {    mat[i][0]=0;
+                if(j!=0)
+                    mat[0][j]=0;
+                else
+                    col=0;
                 }
             }
         }
         
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=1;j<m;j++)
             {
-                if(d1[i]==1 || d2[j]==1)
+                if(mat[i][0]==0 || mat[0][j]==0)
                     mat[i][j]=0;
             }
+        }
+        
+        if(mat[0][0]==0)
+        {
+            for(int i=0;i<m;i++)
+                mat[0][i]=0;
+        }
+        if(col==0)
+        {
+            for(int i=0;i<n;i++)
+                mat[i][0]=0;
         }
     }
 };
