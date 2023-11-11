28f6087832dce4c32090ae5610968bb03f19b7ff
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-11-11 21:49:04 +0530

    Time: 4 ms (86.76%), Space: 7.7 MB (43.05%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    if (s.size() != t.size())
+        return false;
+    
+    vector<int> arr(26); // like a hashmap
+
+    for (int i = 0; i < s.size(); i++)
+        arr[s[i] - 'a']++;
+
+    for (int i = 0; i < t.size(); i++) {
+        int freq = arr[t[i] - 'a'] - 1;
+
+        if (freq < 0)
+            return false;
+        
+        // Update the frequency in the array
+        arr[t[i] - 'a']--;
+    }
+
+    return true;
+}
+
+};
