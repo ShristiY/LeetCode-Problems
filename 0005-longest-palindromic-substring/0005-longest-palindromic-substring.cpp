8ca4285e7af29996c53faa9a6f230dafb40b00b2
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-11-11 22:30:52 +0530

    Time: 755 ms (18.51%), Space: 289.2 MB (11.95%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-       bool solve(string &s, int l, int r){
-        
-        while(l <= r) {
-            if(s[l] != s[r])
-                return false;
-            l++;
-            r--;
-        }
-        
-        return true;
-    }
-    string longestPalindrome(string s) {
-        int n = s.length();
-        
-        int maxlen = INT_MIN;
-        int startingIndex = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                
-                if(solve(s, i, j)) {
-                    if(j-i+1 > maxlen){
-                        startingIndex = i;
-                        maxlen = j-i+1;
-                    }
+
+bool solve(string &s, int l, int r, vector<vector<int>> &dp) {
+    if (l >= r)
+        return true;
+
+    if (dp[l][r] != -1)
+        return dp[l][r];
+
+    return dp[l][r] = solve(s, l + 1, r - 1, dp) && (s[l] == s[r]);
+}
+
+string func(string s, vector<vector<int>> &dp) {
+    int n = s.length();
+
+    int maxlen = INT_MIN;
+    int startingIndex = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            if (solve(s, i, j, dp)) {
+                if (j - i + 1 > maxlen) {
+                    startingIndex = i;
+                    maxlen = j - i + 1;
                 }
-                
             }
         }
-
-        return s.substr(startingIndex, maxlen);
     }
+
+    return s.substr(startingIndex, maxlen);
+}
+
+string longestPalindrome(string s) {
+    int n = s.size();
+    vector<vector<int>> dp(n, vector<int>(n, -1));
+    return func(s, dp);
+}
+
 };
