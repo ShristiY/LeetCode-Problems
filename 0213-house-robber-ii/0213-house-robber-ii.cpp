9c220e8f645c37f1bdbc134656b2fefb8965c02e
ShristiY <99723739+ShristiY@users.noreply.github.com>
2023-11-12 16:15:18 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (24.22%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-       int solve(vector<int>& nums) {
-        int n=nums.size();
-        int prev=nums[0];
-        int prev2=0;
-        for(int i=1;i<n;i++)
-        {
-            int pick=nums[i];
-            if(i>1)
-                pick+=prev2;
-            int notPick=prev;
-            int curr=max(pick,notPick);
-            prev2=prev;
-            prev=curr;
-        }
-        return prev;
-    }
+    int f(int i,vector<int>&nums,vector<int>&dp)//same as the one used in house robber1
+	{
+		if(i==0)
+			return nums[i];
+		if(i<0)
+			return 0;
+     
+     if(dp[i]!=-1)
+     return dp[i];
+     
+    int p=nums[i]+f(i-2,nums,dp);
+    int np=0+f(i-1,nums,dp);
+     
+    return dp[i]=max(p,np);
+}
     int rob(vector<int>& nums) {
-        vector<int>firstIn,lastIn;
-        int n=nums.size();
-        if(n==1) return nums[0];
-        for(int i=0;i<n;i++)
-        {
-            if(i!=0) lastIn.push_back(nums[i]);
-            if(i!=n-1) firstIn.push_back(nums[i]);
-        }
-        int ans1=solve(firstIn);
-        int ans2=solve(lastIn);
-        return max(ans1,ans2);
+         int n=nums.size();
+       
+    vector<int>a;
+    vector<int>b;
+     if(n==1) return nums[0];//edge case
+    for(int i=0;i<n-1;i++)
+    {
+        a.push_back(nums[i]);//skip first element
+    }
+    for(int i=1;i<n;i++)
+    {
+        b.push_back(nums[i]);//or skip last element
+    }
+    vector<int>dp1(a.size(),-1); //design the dp arrray accordingiy
+    vector<int>dp2(b.size(),-1);
+    int d= f(a.size()-1,a,dp1);//passing first skipped element
+    int c= f(b.size()-1,b,dp2);//passing last skipped element
+    return max(c,d);//return maximum of both
     }
 };
