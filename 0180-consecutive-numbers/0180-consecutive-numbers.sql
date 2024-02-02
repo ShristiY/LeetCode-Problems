201bea0e99b55423e22d8c976b1732c4a9b1b630
Shristi Yadav <99723739+ShristiY@users.noreply.github.com>
2024-02-02 16:54:28 +0530

    Time: 962 ms (54.22%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
 
-SELECT
-    DISTINCT Num AS ConsecutiveNums
-FROM
-    (
-        SELECT
-            Num,
-            @cnt := IF(@prev = (@prev := Num), @cnt + 1, 1) AS freq
-        FROM
-            Logs, (SELECT @cnt := 0, @prev := (SELECT Num FROM Logs LIMIT 1)) AS c
-    ) AS n
-WHERE
-    freq > 2;
+
+SELECT DISTINCT Num as ConsecutiveNums
+FROM(
+    SELECT Num,
+    @cnt:= IF (@prev= (@prev:=Num),@cnt+1,1) AS freq
+    
+    FROM Logs,
+    (SELECT @cnt:=0, @prev:=(SELECT Num FROM Logs LIMIT 1)) AS c
+
+) AS n
+WHERE freq>2
+
